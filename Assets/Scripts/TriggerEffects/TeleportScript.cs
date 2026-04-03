9cd7e9c298cca217686a14f7c17b3dc8ab56dd7e
CrimsonsTempest <rafaeldevenang@gmail.com>
2026-04-03 20:38:39 +0700

    update

--- Assets/Scripts/TriggerEffects/TeleportScript.cs
+++ Assets/Scripts/TriggerEffects/TeleportScript.cs
@@ -6,17 +6,27 @@ public class TeleportScript : EffectScript
     public GameObject to;
     public SoundtrackScript sts;
     public SoundEffectScript ses;
+    LogicScript logicScript;
 
 
+
+
+void Start ()
+    {
+    logicScript = FindObjectOfType<LogicScript>();
+
+    }
 
     public override void EffectTrigger()
     {
         Teleport();
     }
+
     void Teleport()
     {
               GameObject player = GameObject.FindGameObjectWithTag("Player");
         player.transform.position = to.transform.position;
+        logicScript.insideHouse =!logicScript.insideHouse;
         if (sts != null)
         {
             sts.togglesound();
