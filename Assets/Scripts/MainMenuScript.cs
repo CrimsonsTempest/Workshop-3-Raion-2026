f6ada9fdc84222f6fa527f6755e9c1ab07ba20e0
CrimsonsTempest <rafaeldevenang@gmail.com>
2026-04-14 19:46:41 +0700

    no inspector button event

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -10,6 +10,12 @@ public class MainMenuScript : MonoBehaviour
 
     public Button exitButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playButton = GameObject.Find("Play").GetComponent<Button>();
+        exitButton = GameObject.Find("Exit").GetComponent<Button>();
+    }
+
     void OnEnable()
     {
         playButton.onClick.AddListener(Play);
